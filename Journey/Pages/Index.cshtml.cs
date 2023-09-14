c22364e91e749ecb4ceb7401c788dc380955570c
Jennifer Marsman <jennmar@microsoft.com>
2023-09-13 20:45:05 -0400

    Working code to parse the navigation json file and display the title and text of the first step on the page

--- Journey/Pages/Index.cshtml.cs
+++ Journey/Pages/Index.cshtml.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace Journey.Pages
 {
     public class IndexModel : PageModel
     {
+        Dictionary<string, Step> steps;
+        string currentStep = "FirstStep";
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,7 +20,49 @@ namespace Journey.Pages
 
         public void OnGet()
         {
+            // TODO: Pull the identity and current step from somewhere
+            // TODO: Based on identity, retrieve information (hardcoding for now)
+            string FirstName = "Mary";
+            //string Address = "100 Peachtree Lane";
+            //string City = "Atlanta";
+            //string State = "GA";
+            string ZipCode = "30303";
+            currentStep = "FirstStep";
 
+            // TODO: Test and ensure that the navigation json loads only once and is held in memory
+            LoadJson();
+            LoadPage(currentStep);
+
+            // TODO: reward screen separate?  or add image to step layout?  
+            // TODO: how do we accommodate feedback from peer mentor?  Notification?  
+        }
+
+        private void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("navigation.json"))
+            {
+                string strJson = r.ReadToEnd();
+                steps = JsonConvert.DeserializeObject<Dictionary<string, Step>>(strJson);
+            }
+        }
+
+        private void LoadPage(string step)
+        {
+            ViewData["Heading"] = steps[step].Title;
+            // TODO: need to format Text to include Input (name and such) in the output
+            ViewData["Text"] = steps[step].Text;
+
+            // TODO: dynamically generate buttons in html layout
+            // TODO: use Functions
+            // TODO: use Rewards
+        }
+
+        public void OnButtonPress()
+        {
+            // TODO: Store off user's progress in journey - step name
+            // TODO: Redraw page based on next step and what button was clicked
+            // TODO: change currentStep
+            // TODO: call LoadPage() with new step
         }
     }
 }
---
+++ Journey/Step.cs
@@ -0,0 +1,27 @@
+﻿namespace Journey
+{
+    public class Step
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string? Inputs { get; set; }
+        public Responses Responses { get; set; }
+        public Functions? Functions { get; set; }
+        //public string? Functions { get; set; }
+        public string? Reward { get; set; }
+    }
+
+    public class Responses
+    {
+        public string Name { get; set; }
+        public string Next { get; set; }
+
+    }
+
+    // TODO: consider making this a string and not class if not needed
+    public class Functions
+    {
+        public string Name { get; set; }
+    }
+
+}
